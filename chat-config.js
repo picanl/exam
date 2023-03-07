22fe1367920f178b3b0cb849b7b170ffc3c3136f
picanl <36163190+picanl@users.noreply.github.com>
2023-03-07 12:29:51 +0100

    Add files via upload

---
+++ chat-config.js
@@ -0,0 +1,4 @@
+const chatconfig = {
+    CHAT_PATH_ENCRYPTED: 'CurCTSSvU+Qco7kaTvfozU0UQG6pYax77s8eOTZveeZ/fakSKNfPzT55xneAYBEcm/Mel+7ail59vOGDm5KC5ToqP5ugXkB1T1AfZgBR7TcGg/Hcsr7xu8sZtwa4MOJ/',
+    DEVMODE_ACTIVE: false,
+  }
